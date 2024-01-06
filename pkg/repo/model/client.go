ac3c612a6e274ed44b8845483d2a776d7c3de288
singchia <singchia@163.com>
2024-01-06 22:00:34 +0800

    add client rpc related repo logic

--- pkg/repo/model/client.go
+++ pkg/repo/model/client.go
@@ -1,7 +1,8 @@
 package model
 
 const (
-	TnClients = "clients"
+	TnClients    = "clients"
+	TnClientRPCs = "client_rpcs"
 )
 
 type Client struct {
@@ -13,4 +14,14 @@ type Client struct {
 
 func (Client) TableName() string {
 	return TnClients
+}
+
+type ClientRPC struct {
+	RPC        string `gorm:"column:rpc;index:idx_rpc"`
+	ClientID   uint64 `gorm:"column:client_id;index:idx_client_id"`
+	CreateTime int64  `gorm:"column:create_time;index:create_time"`
+}
+
+func (ClientRPC) TableName() string {
+	return TnClientRPCs
 }
