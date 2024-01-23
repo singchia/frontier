17b52fe04503f172ff04c94c83a8641b7ffc014a
singchia <singchia@163.com>
2024-01-23 20:02:41 +0800

    servicebound: add basic manager

---
+++ pkg/servicebound/service_dataplane.go
@@ -0,0 +1,40 @@
+package servicebound
+
+import (
+	"github.com/singchia/geminio"
+	"k8s.io/klog/v2"
+)
+
+func (sm *serviceManager) acceptStream(stream geminio.Stream) {
+	serviceID := stream.ClientID()
+	streamID := stream.StreamID()
+	service := stream.Meta()
+	klog.V(5).Infof("service accept stream, serviceID: %d, streamID: %d, service: %s", serviceID, streamID, service)
+
+	// cache
+	sm.streams.MSet(serviceID, streamID, stream)
+	// exchange to edge
+	if sm.exchange != nil {
+		sm.exchange.StreamToEdge(stream)
+	}
+}
+
+func (sm *serviceManager) closedStream(stream geminio.Stream) {
+	serviceID := stream.ClientID()
+	streamID := stream.StreamID()
+	service := stream.Meta()
+	klog.V(5).Infof("service closed stream, serviceID: %d, streamID: %d, service: %s", serviceID, streamID, service)
+	// cache
+	sm.streams.MDel(serviceID, streamID)
+	// when the stream ends, the exchange can be noticed by functional error, so we don't update exchange
+}
+
+// forward to exchange
+func (sm *serviceManager) forward(end geminio.End) {
+	serviceID := end.ClientID()
+	service := end.Meta()
+	klog.V(5).Infof("service forward stream, serviceID: %d, service: %s", serviceID, service)
+	if sm.exchange != nil {
+		sm.exchange.ForwardToService(end)
+	}
+}
---
+++ pkg/servicebound/service_manager.go
@@ -0,0 +1,163 @@
+package servicebound
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"net"
+	"os"
+	"sync"
+
+	"github.com/singchia/frontier/pkg/config"
+	"github.com/singchia/frontier/pkg/mapmap"
+	"github.com/singchia/frontier/pkg/repo/dao"
+	"github.com/singchia/frontier/pkg/security"
+	"github.com/singchia/geminio"
+	"github.com/singchia/geminio/delegate"
+	"github.com/singchia/geminio/pkg/id"
+	"github.com/singchia/geminio/server"
+	"github.com/singchia/go-timer/v2"
+	"k8s.io/klog/v2"
+)
+
+type Servicebound interface {
+	ListService() []geminio.End
+	// for management
+	GetService(service string) geminio.End
+	DelSerivces(service string) error
+}
+
+type ServiceInformer interface {
+	ServiceOnline(serviceID uint64, service string, addr net.Addr)
+	ServiceOffline(serviceID uint64, service string, addr net.Addr)
+	ServiceHeartbeat(serviceID uint64, service string, addr net.Addr)
+}
+
+type Exchange interface {
+	// rpc, message and raw io to edge
+	ForwardToService(geminio.End)
+	// stream to edge
+	StreamToEdge(geminio.Stream)
+}
+
+type serviceManager struct {
+	*delegate.UnimplementedDelegate
+
+	informer ServiceInformer
+	exchange Exchange
+	conf     *config.Configuration
+	// serviceID allocator
+	idFactory id.IDFactory
+	// cache
+	// key: serviceID; value: geminio.End
+	services sync.Map
+	// key: serviceID; subkey: streamID; value: geminio.Stream
+	// we don't store stream info to dao, because they may will be too much.
+	streams *mapmap.MapMap
+
+	// dao and repo for services
+	dao *dao.Dao
+	ln  net.Listener
+
+	// timer for all service ends
+	tmr timer.Timer
+}
+
+func newServiceManager(conf *config.Configuration, dao *dao.Dao, informer ServiceInformer,
+	exchange Exchange, tmr timer.Timer) (*serviceManager, error) {
+	listen := &conf.Servicebound.Listen
+	var (
+		ln      net.Listener
+		network string = listen.Network
+		addr    string = listen.Addr
+		err     error
+	)
+
+	sm := &serviceManager{
+		conf:                  conf,
+		tmr:                   tmr,
+		streams:               mapmap.NewMapMap(),
+		dao:                   dao,
+		UnimplementedDelegate: &delegate.UnimplementedDelegate{},
+		// a simple unix timestamp incremental id factory
+		idFactory: id.DefaultIncIDCounter,
+		informer:  informer,
+	}
+
+	if !listen.TLS.Enable {
+		if ln, err = net.Listen(network, addr); err != nil {
+			klog.Errorf("service manager net listen err: %s, network: %s, addr: %s", err, network, addr)
+			return nil, err
+		}
+	} else {
+		// load all certs to listen
+		certs := []tls.Certificate{}
+		for _, certFile := range listen.TLS.Certs {
+			cert, err := tls.LoadX509KeyPair(certFile.Cert, certFile.Key)
+			if err != nil {
+				klog.Errorf("service manager tls load x509 cert err: %s, cert: %s, key: %s", err, certFile.Cert, certFile.Key)
+				continue
+			}
+			certs = append(certs, cert)
+		}
+
+		if !listen.TLS.MTLS {
+			// tls
+			if ln, err = tls.Listen(network, addr, &tls.Config{
+				MinVersion:   tls.VersionTLS12,
+				CipherSuites: security.CiperSuites,
+				Certificates: certs,
+			}); err != nil {
+				klog.Errorf("service manager tls listen err: %s, network: %s, addr: %s", err, network, addr)
+				return nil, err
+			}
+
+		} else {
+			// mtls, require for edge cert
+			// load all ca certs to pool
+			caPool := x509.NewCertPool()
+			for _, caFile := range listen.TLS.CACerts {
+				ca, err := os.ReadFile(caFile)
+				if err != nil {
+					klog.Errorf("service manager read ca cert err: %s, file: %s", err, caFile)
+					return nil, err
+				}
+				if !caPool.AppendCertsFromPEM(ca) {
+					klog.Warningf("service manager append ca cert to ca pool err: %s, file: %s", err, caFile)
+					continue
+				}
+			}
+			if ln, err = tls.Listen(network, addr, &tls.Config{
+				MinVersion:   tls.VersionTLS12,
+				CipherSuites: security.CiperSuites,
+				ClientCAs:    caPool,
+				ClientAuth:   tls.RequireAndVerifyClientCert,
+				Certificates: certs,
+			}); err != nil {
+				klog.Errorf("service manager tls listen err: %s, network: %s, addr: %s", err, network, addr)
+				return nil, err
+			}
+		}
+	}
+	sm.ln = ln
+	return sm, nil
+}
+
+func (sm *serviceManager) Serve() {
+	for {
+		conn, err := sm.ln.Accept()
+		if err != nil {
+			klog.V(4).Infof("service manager listener accept err: %s", err)
+			return
+		}
+		go sm.handleConn(conn)
+	}
+}
+
+func (sm *serviceManager) handleConn(conn net.Conn) error {
+	// options for geminio End
+	opt := server.NewEndOptions()
+	opt.SetTimer(sm.tmr)
+	opt.SetDelegate(sm)
+	// stream handler
+	return nil
+}
