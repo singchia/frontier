91b655bf4bca94f2af4aa6c51deda68754aa62d8
singchia <singchia@163.com>
2024-03-22 19:42:17 +0800

    config: add commoan config

---
+++ pkg/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+// listen related
+type CertKey struct {
+	Cert string `yaml:"cert"`
+	Key  string `yaml:"key"`
+}
+
+type TLS struct {
+	Enable             bool      `yaml:"enable"`
+	MTLS               bool      `yaml:"mtls"`
+	CACerts            []string  `yaml:"ca_certs"`             // ca certs paths
+	Certs              []CertKey `yaml:"certs"`                // certs paths
+	InsecureSkipVerify bool      `yaml:"insecure_skip_verify"` // for client use
+}
+
+type Listen struct {
+	Network string `yaml:"network"`
+	Addr    string `yaml:"addr"`
+	TLS     TLS    `yaml:"tls"`
+}
