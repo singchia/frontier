0357376af476bc91781abf665def1907aa78d5a6
singchia <singchia@163.com>
2024-03-23 23:24:03 +0800

    api: update frontlas and frontier apis

--- pkg/frontier/controlplane/server/grpc.go
+++ pkg/frontier/controlplane/server/grpc.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
-	v1 "github.com/singchia/frontier/api/controlplane/v1"
+	v1 "github.com/singchia/frontier/api/controlplane/frontier/v1"
 )
 
 func NewGRPCServer(ln net.Listener, svc v1.ControlPlaneServer) *grpc.Server {
